16bc88e1c9e48b39c0346a980ca7c3ad5e1da6ba
Santiago Calzolari <santyxd321@gmail.com>
2025-09-21 00:43:25 -0300

    cambios setings

--- tienda/settings.py
+++ tienda/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-a*rrx*8n6q6l=v%y4ze45
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '127.0.0.1,localhost').split(',')
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'proyectoecommerce.onrender.com']
+
 
 
 
